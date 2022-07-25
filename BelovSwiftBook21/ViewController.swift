2abd0778f3bb7c7b9034abb7c09bedc05db412ac
DICSA <belovrl1@gmail.com>
2022-07-25 22:07:40 +0300

    Initial Commit

---
+++ BelovSwiftBook21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BelovSwiftBook21
+//
+//  Created by Белов Руслан on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
